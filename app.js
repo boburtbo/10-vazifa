e0e9bb89a6009cfbe2826b64a5ad2e54dfdf9635
boburbo <bobur20081608@gmail.com>
2024-04-03 23:18:48 +0500

    10vazifa

---
+++ app.js
@@ -0,0 +1,50 @@
+const countriesContainer = document.querySelector(".countries-container");
+
+export function loader(state) {
+  if (state) {
+    overlay.classList.remove("d-none");
+  } else {
+    overlay.classList.add("d-none");
+  }
+}
+
+const getData = async () => {
+  loader(true);
+  await fetch("https://restcountries.com/v3.1/all")
+    .then((res) => res.json())
+    .then((data) => {
+      renderCountries(data);
+      allCountriesData = data;
+      loader(false);
+    });
+};
+
+getData();
+
+filterByRegion.addEventListener("change", (e) => {
+  fetch(`https://restcountries.com/v3.1/region/${filterByRegion.value}`)
+    .then((res) => res.json())
+    .then(renderCountries);
+});
+
+function renderCountries(data) {
+  countriesContainer.innerHTML = "";
+  data.forEach((country) => {
+    const countryCard = document.createElement("a");
+    countryCard.classList.add("country-card");
+    countryCard.href = `./country.html?name=${country.name.common}`;
+    countryCard.innerHTML = `
+          <img src="${country.flags.svg}" alt="${country.name.common} flag" />
+          <div class="card-text">
+              <h3 class="card-title">${country.name.common}</h3>
+              <p><b>Population: </b>${country.population.toLocaleString(
+                "en-EH"
+              )}</p>
+             
+              <p><b>Capital: </b>${country.capital?.[0]}</p>
+          </div>
+  `;
+    console.log(countryCard.hreg);
+    countriesContainer.append(countryCard);
+  });
+}
